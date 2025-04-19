d0b9dd031b46a7f4248473889cea326e91b30fdb
Багаутдинов-Ринат <tensal@bk.ru>
2025-04-19 18:59:58 +0300

    сжимаем,сохраняем и обрабатываем

--- image_compressor.py
+++ image_compressor.py
@@ -22,3 +22,19 @@ class ImageCompressor:
             self.__quality = value
         else:
             raise ValueError("Качество должно быть в диапазоне от 1 до 100")
+        
+
+    def compress_image(self, input_path: str, output_path: str) -> None:
+        with Image.open(input_path) as img:
+            img.save(output_path, "HEIF", quality=self.__quality)
+        print(f"Сжато: {input_path} -> {output_path}")
+    
+
+
+    def process_directory(self, directory: str) -> None:
+        for root, _, files in os.walk(directory):
+            for file in files:
+                if file.lower().endswith(self.supported_formats):
+                    input_path = os.path.join(root, file)
+                    output_path = os.path.splitext(input_path)[0] + '.heic'
+                    self.compress_image(input_path, output_path)
