629785b0cf7275a072dba77a7f4b42c0680fe47f
Багаутдинов-Ринат <tensal@bk.ru>
2025-04-19 19:01:59 +0300

    основная функция обработки

--- image_compressor.py
+++ image_compressor.py
@@ -30,7 +30,6 @@ class ImageCompressor:
         print(f"Сжато: {input_path} -> {output_path}")
     
 
-
     def process_directory(self, directory: str) -> None:
         for root, _, files in os.walk(directory):
             for file in files:
@@ -38,3 +37,26 @@ class ImageCompressor:
                     input_path = os.path.join(root, file)
                     output_path = os.path.splitext(input_path)[0] + '.heic'
                     self.compress_image(input_path, output_path)
+
+
+
+def main(input_path: str) -> None:
+    register_heif_opener()
+    input_path = input_path.strip('"')
+    
+    compressor = ImageCompressor(quality=50)
+    
+    if os.path.exists(input_path):
+        if os.path.isfile(input_path):
+            print(f"Обрабатываем файл: {input_path}")
+            output_path = os.path.splitext(input_path)[0] + '.heic'
+            compressor.compress_image(input_path, output_path)
+        elif os.path.isdir(input_path):
+            print(f"Обрабатываем директорию: {input_path}")
+            compressor.process_directory(input_path)
+    else:
+        print("Указанный путь не существует")
+
+if __name__ == "__main__":
+    user_input = input("Введите путь к файлу или директории: ")
+    main(user_input)
